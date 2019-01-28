dbaf3f8ad84cf172ae46957b710bd988f320f024
Oli <oliver@fraclog.co>
2019-01-28 11:33:03 +0000

    actually export new DateRangeFilter

--- packages/react-data-grid-addons/src/cells/headerCells/filters/index.js
+++ packages/react-data-grid-addons/src/cells/headerCells/filters/index.js
@@ -1,10 +1,12 @@
 import NumericFilter from './NumericFilter';
+import DateRangeFilter from './DateRangeFilter';
 import AutoCompleteFilter from './AutoCompleteFilter';
 import MultiSelectFilter from './MultiSelectFilter';
 import SingleSelectFilter from './SingleSelectFilter';
 
 const Filters = {
   NumericFilter: NumericFilter,
+  DateRangeFilter: DateRangeFilter,
   AutoCompleteFilter: AutoCompleteFilter,
   MultiSelectFilter: MultiSelectFilter,
   SingleSelectFilter: SingleSelectFilter
